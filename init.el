d8ae3e0d59443e8fbddfa9b2eeed1723a7a6c34f
marshall flax <marshall.flax@gmail.com>
2017-07-13 08:18:34 -0400

    Continue moving stuff around

--- init.el
+++ init.el
@@ -1,10 +1,5 @@
 (package-initialize)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,6 +24,7 @@
  '(elm-tags-exclude-elm-stuff nil)
  '(emacspeak-auditory-icon-function (quote emacspeak-queue-auditory-icon))
  '(eww-bookmarks-directory "~/gits/personal")
+ '(fill-column 80)
  '(gc-cons-threshold 20000000)
  '(haskell-stylish-on-save t)
  '(ido-max-prospects 3)
@@ -38,7 +34,9 @@
  '(line-number-mode t)
  '(mac-default-speech-rate 300)
  '(magit-diff-use-overlays nil)
+ '(mouse-yank-at-point t)
  '(nrepl-sync-request-timeout 30)
+ '(ns-pop-up-frames nil)
  '(org-ellipsis "⤵")
  '(org-html-checkbox-type (quote unicode))
  '(org-log-done (quote time))
@@ -56,7 +54,8 @@
     (recover-buffers diminish exec-path-from-shell flycheck company circe org-bullets magit rainbow-delimiters)))
  '(show-paren-delay 0.0)
  '(text-scale-mode-step 1.1)
- '(vc-follow-symlinks t))
+ '(vc-follow-symlinks t)
+ '(visible-bell t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -67,6 +66,3 @@
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
-(company-mode)
-(winner-mode 1)
-(require 'ido) (ido-mode t)
